6a10a4a6c87b1236f1f5b92d9b202585762ee518
johnsonhung906 <khhung906@gmail.com>
2021-11-05 21:16:03 +0800

    massage handle

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# 載入需要的模組
+# import needed module
 from __future__ import unicode_literals
 import os
 from flask import Flask, request, abort
@@ -8,7 +8,6 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSend
 import configparser
 import json
 app = Flask(__name__)
-
 
 
 # LINE BOT INFORMATION
@@ -29,24 +28,48 @@ def callback():
         abort(400)
     return 'Nice'
 
-
-def isGreeting(msg):
-    greeting = ["hello", "hi", "哈囉", "嗨", "海螺"]
-    flag = False
-    for m in greeting:
-        if msg.lower() in m or m in msg.lower():
-            flag = True
-    return flag
-
-@handler.add(MessageEvent, message=TextMessage)
-def echo(event):
-    msg = event.message.text
-    profile = line_bot_api.get_profile(event.source.user_id)
-    if isGreeting(msg):
-        line_bot_api.reply_message(
+def ScheduleHandler(event, profile):
+    line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="Hello " + profile.display_name[1:])
         )
+def FoodHandler(event, profile):
+    line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Hello " + profile.display_name[1:])
+        )
+def SiteHandler(event, profile):
+    line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Hello " + profile.display_name[1:])
+        )
+
+def GameHandler(event, profile):
+    line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Hello " + profile.display_name[1:])
+        )
+
+def GGHandler(event, profile):
+    line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="You GG")
+        )
+
+@handler.add(MessageEvent, message=TextMessage)
+def messageHandler(event):
+    msg = event.message.text
+    profile = line_bot_api.get_profile(event.source.user_id)
+    if msg == "行程表/導遊":
+        ScheduleHandler(event, profile)
+    elif msg == "美食":
+        FoodHandler(event, profile)
+    elif msg == "景點介紹/預約":
+        SiteHandler(event, profile)
+    elif msg == "遊戲":
+        GameHandler(event, profile)
+    else:
+        GGHandler(event, profile)
 
 if __name__ == "__main__":
     app.run()
