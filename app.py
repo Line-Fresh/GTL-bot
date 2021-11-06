57331b9504b2abc3aff9d7da25d2b98b2c4b79dc
fhj0604 <b09902106@g.ntu.edu.tw>
2021-11-06 20:15:35 +0800

    guide update

--- app.py
+++ app.py
@@ -4,14 +4,11 @@ import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, FlexSendMessage, DatetimePickerAction, QuickReply, QuickReplyButton
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, FlexSendMessage, DatetimePickerAction, QuickReply, QuickReplyButton, PostbackEvent
 import configparser
 import json
 import datetime
 app = Flask(__name__)
-
-
-
 
 
 
@@ -21,6 +18,8 @@ config.read('config.ini')
 line_bot_api = LineBotApi(config['line-bot']['channel_access_token'])
 handler = WebhookHandler(config['line-bot']['channel_secret'])
 
+
+active_user = {} #temprorary database
 
 # LINE BOT REPLY
 @app.route("/callback", methods=['POST'])
@@ -42,12 +41,24 @@ def ScheduleHandler(event, profile, msg):
                 FlexSendMessage(alt_text="Test", contents=FlexMessage)
             )
     elif msg == "挑選導遊" or msg == "重新選擇":
+        print(event.reply_token)
         FlexMessage = json.load(open('jsonfile/tour.json','r',encoding='utf-8'))
         line_bot_api.reply_message(
                 event.reply_token,
                 FlexSendMessage(alt_text="Test", contents=FlexMessage)
             )
+    
     elif msg[:2] == "選擇":
+        guide = msg[2:]
+        FlexMessage = json.load(open('jsonfile/reservation.json','r',encoding='utf-8'))
+        active_user[event.source.user_id] = {}
+        active_user[event.source.user_id]["guide"] = guide
+        print(active_user)
+        line_bot_api.reply_message(
+                event.reply_token,
+                FlexSendMessage(alt_text="Test", contents=FlexMessage[guide])
+            )
+    elif msg == "預約完成":
         guide = msg[2:]
         FlexMessage = json.load(open('jsonfile/guide.json','r',encoding='utf-8'))
         FlexMessage[guide]["body"]["contents"][2]["contents"][0]["contents"][1]["text"] = profile.display_name
@@ -55,26 +66,11 @@ def ScheduleHandler(event, profile, msg):
                 event.reply_token,
                 FlexSendMessage(alt_text="Test", contents=FlexMessage[guide])
             )
-def FoodHandler(event, profile):
-    message=TextSendMessage(
-        text="文字訊息",
-        quick_reply=QuickReply(
-            items=[
-                QuickReplyButton(
-                    action=DatetimePickerAction(label="時間選擇",data="時間選擇",mode='datetime')
-                    )
-                ]
-            )
-    )
+def FoodHandler(event, profile):    
     line_bot_api.reply_message(
-                event.reply_token,
-                message
-            )
-
-    # line_bot_api.reply_message(
-    #         event.reply_token,
-    #         TextSendMessage(text="Hello " + profile.display_name[1:])
-    #     )
+            event.reply_token,
+            TextSendMessage(text="Hello " + profile.display_name[1:])
+        )
 def SiteHandler(event, profile):
     line_bot_api.reply_message(
             event.reply_token,
@@ -93,6 +89,7 @@ def GGHandler(event, profile):
             TextSendMessage(text="You GG")
         )
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def messageHandler(event):
     msg = event.message.text
@@ -108,5 +105,26 @@ def messageHandler(event):
     else:
         GGHandler(event, profile)
 
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    uid = event.source.user_id
+    active_user[uid]["time"] = event.postback.params['date']
+
+    user_name = line_bot_api.get_profile(uid).display_name
+    guide = active_user[uid]["guide"]
+    time = active_user[uid]["time"]
+    FlexMessage = json.load(open('jsonfile/guide.json','r',encoding='utf-8'))
+    FlexMessage[guide]["body"]["contents"][2]["contents"][0]["contents"][1]["text"] = user_name
+    FlexMessage[guide]["body"]["contents"][2]["contents"][1]["contents"][1]["text"] = time
+    line_bot_api.push_message(
+        uid, 
+        TextSendMessage(text="預約完成")
+    )
+    line_bot_api.reply_message(
+            event.reply_token,
+            FlexSendMessage(alt_text="Test", contents=FlexMessage[guide])
+        )
+    
 if __name__ == "__main__":
     app.run()
